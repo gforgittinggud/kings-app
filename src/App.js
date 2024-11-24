ef23b061ea97e292e1dd62255b634b63514135e8
Kafka <107980409+gforgittinggud@users.noreply.github.com>
2024-11-24 18:25:17 +0800

    Init repo
    init repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Scrollbar from "./components/Scrollbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Scrollbar />
+        <nav>
+          <ul>
+            <li>
+              <a href="#/">KINGS CM</a>
+            </li>
+            <li>
+              <a href="#section1">Section 1</a>
+            </li>
+            <li>
+              <a href="#section2">Section 2</a>
+            </li>
+            <li>
+              <a href="#section3">Section 3</a>
+            </li>
+            <li>
+              <a href="#section4">Section 4</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      <div className="content">
+        <section id="section1" className="section">
+          <h2>Section 1</h2>
+          <p>Information about Section 1.</p>
+        </section>
+        <section id="section2" className="section">
+          <h2>Section 2</h2>
+          <p>Information about Section 2.</p>
+        </section>
+        <section id="section3" className="section">
+          <h2>Section 3</h2>
+          <p>Information about Section 3.</p>
+        </section>
+        <section id="section4" className="section">
+          <h2>Section 4</h2>
+          <p>Information about Section 4.</p>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/Scrollbar.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+
+function Scrollbar() {
+  //   return <>hello</>;
+  const [scrollPercent, setScrollPercent] = useState(0);
+  console.log("Scrollbar rendered");
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollY = window.scrollY;
+
+      const scrollPercentage =
+        (scrollY / (documentHeight - windowHeight)) * 100;
+      console.log(scrollPercentage);
+      console.log(
+        `scrollY: ${scrollY}, documentHeight: ${documentHeight}, windowHeight: ${windowHeight}`
+      );
+      setScrollPercent(scrollPercentage);
+    };
+    console.log("useEffect is running");
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      //   window.addEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      id="progress-container"
+      style={{
+        height: "8px",
+        width: "100%",
+        backgroundColor: "blue",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+      }}
+    >
+      <div
+        className="progress-fill"
+        style={{
+          height: "100%",
+          backgroundColor: "green",
+          width: `${scrollPercent}%`,
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default Scrollbar;
