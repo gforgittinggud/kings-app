45f337f2693d238f6666db273984f799192d2498
Kafka <107980409+gforgittinggud@users.noreply.github.com>
2025-02-25 15:55:56 +0800

    Change check page loads

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Load test", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ENGINE PROPULSION CONTROLS/i);
   expect(linkElement).toBeInTheDocument();
 });
